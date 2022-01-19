17888b8eed0ba36146f5ee4d062fa3cdd13e49e4
pauline-wim <linette.wim@hotmail.com>
2022-01-19 15:14:48 +0100

    Add list containing details of each countries

--- front/script.js
+++ front/script.js
@@ -3,7 +3,16 @@ function getAllCountries() {
         url: "https://restcountries.com/v3.1/all",
         success: function (data) {
             data.forEach((country) => {
-                console.log(country.name.common);
+                // console.log(country);
+                if (country.capital === undefined) {
+                    $("#countries-list").append(
+                        `<li><span>${country.name.common}<span>, ${country.continents}, ${country.flag}</li>`
+                    );
+                } else {
+                    $("#countries-list").append(
+                        `<li><span>${country.name.common}<span>, ${country.capital}, ${country.continents}, ${country.flag}</li>`
+                    );
+                }
             });
         },
     });
